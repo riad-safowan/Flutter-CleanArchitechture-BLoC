dcd111b762676999c183022e96f07850c9760c00
Riad Safowan <riad.safowan@northsouth.edu>
2022-09-10 13:57:23 +0600

    BloC pattern implemented

---
+++ lib/features/number_trivia/presentation/bloc/number_trivia_bloc.dart
@@ -0,0 +1,75 @@
+import 'package:bloc/bloc.dart';
+import 'package:equatable/equatable.dart';
+
+import '../../../../core/errors/failures.dart';
+import '../../../../core/usecases/usecase.dart';
+import '../../../../core/utils/input_converter.dart';
+import '../../domain/entities/number_trivia.dart';
+import '../../domain/usecases/get_concreate_number_trivia.dart';
+import '../../domain/usecases/get_random_number_trivia.dart';
+
+part 'number_trivia_event.dart';
+part 'number_trivia_state.dart';
+
+const String SERVER_FAILURE_MESSAGE = 'Server Failure';
+const String CACHE_FAILURE_MESSAGE = 'Cache Failure';
+const String INVALID_INPUT_FAILURE_MESSAGE =
+    'Invalid Input - The number must be a positive integer or zero.';
+
+class NumberTriviaBloc extends Bloc<NumberTriviaEvent, NumberTriviaState> {
+  final GetConcreteNumberTrivia getConcreteNumberTrivia;
+  final GetRandomNumberTrivia getRandomNumberTrivia;
+
+  final InputConverter inputConverter;
+
+  NumberTriviaBloc(
+      {required this.getConcreteNumberTrivia,
+      required this.getRandomNumberTrivia,
+      required this.inputConverter})
+      : super(NumberTriviaInitial()) {
+    on<NumberTriviaEvent>((event, emit) {});
+
+    @override
+    Stream<NumberTriviaState> mapEventToState(
+      NumberTriviaEvent event,
+    ) async* {
+      if (event is GetTriviaForConcreteNumber) {
+        final inputEither =
+            inputConverter.stringToUnsignedInteger(event.numberString);
+        inputEither.fold(
+          (failure) async* {
+            yield Error(message: 'error');
+          },
+          (integer) async* {
+            yield Loading();
+            final failureOrTrivia =
+                await getConcreteNumberTrivia(Params(number: integer));
+
+            failureOrTrivia.fold(
+              (failure) => Error(message: _mapFailureToMessage(failure)),
+              (trivia) => Loaded(trivia: trivia),
+            );
+          },
+        );
+      } else if (event is GetTriviaForRandomNumber) {
+        yield Loading();
+        final failureOrTrivia = await getRandomNumberTrivia(NoParams());
+        failureOrTrivia.fold(
+          (failure) => Error(message: _mapFailureToMessage(failure)),
+          (trivia) => Loaded(trivia: trivia),
+        );
+      }
+    }
+  }
+
+  String _mapFailureToMessage(Failure failure) {
+    switch (failure.runtimeType) {
+      case ServerFailure:
+        return SERVER_FAILURE_MESSAGE;
+      case CacheFailure:
+        return CACHE_FAILURE_MESSAGE;
+      default:
+        return 'Unexpected error';
+    }
+  }
+}
